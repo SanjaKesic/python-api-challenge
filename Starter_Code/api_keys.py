7dc9dfa60a4a367e6ee2dff4f7d269c0ab9a182a
SanjaKesic <125163988+SanjaKesic@users.noreply.github.com>
2023-07-29 17:41:26 -0500

    Update api_keys.py

--- Starter_Code/api_keys.py
+++ Starter_Code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "bef8ec74a66381fcf64316b729bebc5e"
+weather_api_key = ""
 
 # Geoapify API Key
-geoapify_key = "fd248022b9014fe8801a775a118c41da"
+geoapify_key = ""
